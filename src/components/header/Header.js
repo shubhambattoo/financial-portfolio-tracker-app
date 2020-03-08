8b9041af107b20ee7e3a13357d73c27f74676a47
shubhambattoo <shubham.battoo@hotmail.com>
2020-03-08 13:25:28 +0530

    added firebase hosting

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import './Header.scss'
+import './Header.scss';
 
 export default function Header() {
+  const day = new Date().getDay();
   return (
     <>
       <header className="app-header">
-        <a href="/" className="brand-link"> Finance Portfolio Tracker </a>
+        <a href="/" className="brand-link">
+          {' '}
+          Finance Portfolio Tracker{' '}
+        </a>
       </header>
+      {day === 0 || day === 6 ? (
+        <div style={{ textAlign: 'center' }}>
+          * Since it is a weekend all the stock closing prices are taken from
+          Friday.
+        </div>
+      ) : null}
     </>
   );
 }
